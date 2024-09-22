a5b3656457ef571d56a0ed855799bdbf72271654
kimunao <kim.u.k7526@gmail.com>
2024-09-22 12:47:56 +0900

    初回コミット（プロジェクト作成のみ状態）

---
+++ AppPracticeOne/src/main/java/com/example/demoappone/AppPracticeOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoappone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPracticeOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPracticeOneApplication.class, args);
+	}
+
+}
